f5b83c90c3fdd3c5ef0711325e6a8732bd851967
manuelsanchez2 <manusansan22@gmail.com>
2020-09-07 14:35:00 +0200

    Add json-server

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const app = express();
+const jsonServer = require("json-server");
 const morgan = require("morgan");
 const path = require("path");
-
-const app = express();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
 app.use(morgan("common"));
 
 app.use(express.static(path.join(__dirname, "client/build")));
+app.use(middlewares);
+app.use(router);
 
 app.use(
   "/storybook",
