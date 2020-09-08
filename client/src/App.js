f02d1e9af9f3d3662a82ac833f3a202a978be473
manuelsanchez2 <manusansan22@gmail.com>
2020-09-08 12:07:51 +0200

    Fix the problem with the servers and concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editeee <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
