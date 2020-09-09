f9e4fb7f4e377830d556b05bc0b9c91f48541f7e
manuelsanchez2 <manusansan22@gmail.com>
2020-09-09 11:23:49 +0200

    Remove some unnecesary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import GlobalCSS from "./GlobalCSS";
 
 // import {
@@ -12,23 +10,10 @@ import GlobalCSS from "./GlobalCSS";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalCSS />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editeee <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header className="App-header">Hola</header>
+    </>
   );
 }
 
