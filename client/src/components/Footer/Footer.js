63396286e01f4dc6fca50fc3fe16a59770efd877
manuelsanchez2 <manusansan22@gmail.com>
2020-09-11 12:30:47 +0200

    Add the login icon the footer and the rest of the icons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-import doichelivingSrc from "../../assets/images/doicheliving-logo.png";
+// import { Link } from "react-router-dom";
+import homeSrc from "../../assets/images/footer-home.svg";
+import mapSrc from "../../assets/images/footer-map.svg";
+import searchSrc from "../../assets/images/footer-search.svg";
+import profileSrc from "../../assets/images/footer-profile.svg";
 
 const StyledFooter = styled.nav`
   box-shadow: var(--shadow-up);
@@ -16,10 +20,18 @@ const StyledFooter = styled.nav`
 const Footer = () => {
   return (
     <StyledFooter>
-      <img src={doichelivingSrc} alt="" />
-      <img src={doichelivingSrc} alt="" />
-      <img src={doichelivingSrc} alt="" />
-      <img src={doichelivingSrc} alt="" />
+      <a href="/home">
+        <img src={homeSrc} alt="Home" />
+      </a>
+      <a href="/map">
+        <img src={mapSrc} alt="Map" />
+      </a>
+      <a href="/search">
+        <img src={searchSrc} alt="Search Icon" />
+      </a>
+      <a href="/profile">
+        <img src={profileSrc} alt="Profile icon" />
+      </a>
     </StyledFooter>
   );
 };
