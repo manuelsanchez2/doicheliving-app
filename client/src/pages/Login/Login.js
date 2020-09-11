5aa2231dddf5f04499890ccdbded880291602afd
manuelsanchez2 <manusansan22@gmail.com>
2020-09-11 12:01:52 +0200

    Create the page component

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <Header />
+      <div className="main--login">HOLA</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
