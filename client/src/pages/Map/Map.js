af76f75ee71cc7366e60570dc31bcff89b3955a5
manuelsanchez2 <manusansan22@gmail.com>
2020-09-16 09:06:14 +0200

    Remove unnecessary code and add the possibility of adding a new marker

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -7,7 +7,8 @@ import { listSpots } from "../../api/spots";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import styled from "@emotion/styled";
-import mapMarker from "../../assets/icons/map-marker-yellow.svg";
+import mapMarkerYellow from "../../assets/icons/map-marker-yellow.svg";
+import mapMarkerRed from "../../assets/icons/map-marker-red.svg";
 // import returnSrc from "../../assets/icons/arrow.svg";
 
 const StyledMapContainer = styled.div`
@@ -17,27 +18,12 @@ const StyledMapContainer = styled.div`
   justify-content: center;
   overflow: auto;
   background: var(--color-yellow);
-
-  /* img {
-    max-height: 5rem;
-    max-width: 5rem;
-  }
-
-  button {
-    background: var(--color-white);
-    border: 1px solid var(--color-darkgrayborder);
-    border-radius: 50px;
-    width: 48px;
-    height: 48px;
-    margin: 10px 0 0 10px;
-    z-index: 999;
-  } */
 `;
 
 const Map = () => {
-  //   const history = useHistory();
   const [spots, setSpots] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addSpot, setAddSpot] = useState(null);
   const [viewport, setViewport] = useState({
     height: "88.5vh",
     width: "2000",
@@ -68,6 +54,14 @@ const Map = () => {
     })();
   }, []);
 
+  const showAddMarkerPopup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddSpot({
+      latitude,
+      longitude,
+    });
+  };
+
   return (
     <StyledMapContainer>
       <Header />
@@ -85,6 +79,7 @@ const Map = () => {
           //   mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           mapStyle="mapbox://styles/manusanchez2/ckf3tjjnc10a619s7rnb6iqfa"
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
+          onDblClick={showAddMarkerPopup}
         >
           {spots.map((spot) => (
             <>
@@ -104,12 +99,10 @@ const Map = () => {
                 >
                   <img
                     style={{
-                      //   height: `${6 * viewport.zoom}px`,
-                      //   width: `${6 * viewport.zoom}px`,
                       height: "24px",
                       width: "24px",
                     }}
-                    src={mapMarker}
+                    src={mapMarkerYellow}
                     alt="map marker"
                   />
                 </div>
@@ -122,7 +115,7 @@ const Map = () => {
                   closeOnClick={true}
                   dynamicPosition={true}
                   onClose={() => setShowPopup({})}
-                  //   anchor="top"
+                  anchor="top"
                 >
                   <div className="popup">
                     <h3>{spot.title}</h3>
@@ -132,20 +125,58 @@ const Map = () => {
               ) : null}
             </>
           ))}
-          <div>
-            <Geocoder
-              mapRef={mapRef}
-              marker={false}
-              limit={3}
-              minLength={4}
-              placeholder={"Introduce tu destino"}
-              onViewportChange={handleGeocoderViewportChange}
-              mapboxApiAccessToken="pk.eyJ1IjoibWFudXNhbmNoZXoyIiwiYSI6ImNrZDAyMGF6ajBwOGEzMW91YWhpaXBrd3IifQ.DxIOHXiZw3-rvAn5yC8QYw"
-              position="top-right"
-              language="es, en"
-              countries="de"
-            />
-          </div>
+
+          <Geocoder
+            mapRef={mapRef}
+            marker={false}
+            limit={3}
+            minLength={4}
+            placeholder={"Introduce tu destino"}
+            onViewportChange={handleGeocoderViewportChange}
+            mapboxApiAccessToken="pk.eyJ1IjoibWFudXNhbmNoZXoyIiwiYSI6ImNrZDAyMGF6ajBwOGEzMW91YWhpaXBrd3IifQ.DxIOHXiZw3-rvAn5yC8QYw"
+            position="top-right"
+            language="es, en"
+            countries="de"
+          />
+          {addSpot ? (
+            <>
+              <Marker
+                key={addSpot._id}
+                latitude={addSpot.latitude}
+                longitude={addSpot.longitude}
+                offsetLeft={-12}
+                offsetTop={-24}
+              >
+                <div
+                  onClick={() =>
+                    setShowPopup({
+                      [addSpot._id]: true,
+                    })
+                  }
+                >
+                  <img
+                    style={{
+                      height: "24px",
+                      width: "24px",
+                    }}
+                    src={mapMarkerRed}
+                    alt="map marker"
+                  />
+                </div>
+              </Marker>
+              <Popup
+                latitude={addSpot.latitude}
+                longitude={addSpot.longitude}
+                closeButton={false}
+                closeOnClick={true}
+                dynamicPosition={true}
+                anchor="top"
+                onClose={() => setAddSpot(null)}
+              >
+                <h3>Add your shit</h3>
+              </Popup>
+            </>
+          ) : null}
         </ReactMapGL>
       </div>
       <Footer />
