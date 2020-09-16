56e68334de0f79b42f866ba70cafa1fbc9312df3
manuelsanchez2 <manusansan22@gmail.com>
2020-09-16 14:15:09 +0200

    Create the Main for the map page

---
+++ client/src/components/MapMain/MapMain.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import React from "react";
+import PropTypes from "prop-types";
+
+const Main = styled.main`
+  height: 60%;
+  flex: 1;
+  overflow: hidden;
+`;
+
+const MapMain = ({ children }) => {
+  return <Main>{children}</Main>;
+};
+
+export default MapMain;
+
+MapMain.propTypes = {
+  children: PropTypes.node,
+};
