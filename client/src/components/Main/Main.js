faa4e947c43027621e95c53cdb4c621e7564e9bb
manuelsanchez2 <manusansan22@gmail.com>
2020-09-18 11:40:55 +0200

    Create the Main Component

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import PropTypes from "prop-types";
+
+const Main = styled.div`
+  flex: 1;
+  padding: 0 1em 0 1em;
+`;
+
+Main.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Main;
