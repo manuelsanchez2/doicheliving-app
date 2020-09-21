ce71df90378daff3640dfe7f86b57abd819c4bfc
manuelsanchez2 <manusansan22@gmail.com>
2020-09-21 14:45:22 +0200

    Create components for the Menu.js

---
+++ client/src/components/Header/MenuItemImage.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const MenuItemImage = styled.img`
+  width: 1.6rem;
+  height: 1.6rem;
+  margin-left: 0.5rem;
+`;
+
+export default MenuItemImage;
---
+++ client/src/components/Header/MenuItemList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const Menu = styled.ul`
+  margin: 0;
+  list-style: none;
+  display: flex;
+
+  border-right: 1.5px solid var(--color-dark-border);
+
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? "translateX(-20%)" : "translateX(-130%)")};
+  position: absolute;
+  flex-flow: column nowrap;
+  background-color: var(--color-primary);
+
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 85%;
+
+  z-index: 50;
+`;
+
+const MenuItemList = ({ open, children }) => {
+  return <Menu open={open}>{children}</Menu>;
+};
+
+MenuItemList.propTypes = {
+  open: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+export default MenuItemList;
