32efe346c134cb7a09a0c83e15dfae3f3e1a9656
manuelsanchez2 <manusansan22@gmail.com>
2020-09-23 07:18:43 +0200

    Create one more component for the Destination Pages

---
+++ client/src/components/DestinationPageContainer/DestinationPageContainer.js
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+const DestinationPageContainer = styled.div`
+  padding: 0 1em 0 1em;
+  display: flex;
+  flex-direction: column;
+
+  h2,
+  h3 {
+    text-align: center;
+    margin: 0.5rem 0;
+  }
+
+  h3 {
+    font-weight: normal;
+    font-size: 0.9rem;
+    margin-bottom: 1.2rem;
+  }
+
+  p {
+    margin: 0.5rem 0;
+    font-size: 0.6rem;
+    line-height: 0.8rem;
+  }
+`;
+
+export default DestinationPageContainer;
--- client/src/components/DestinationPageMainContent/DestinationPageMainContent.js
+++ client/src/components/DestinationPageMainContent/DestinationPageMainContent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
 import PropTypes from "prop-types";
+import DestinationPageContainer from "../DestinationPageContainer/DestinationPageContainer";
 
 const DestinationImageMapContainer = styled.div`
   display: flex;
-  direction: row;
+  flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;
@@ -17,30 +18,6 @@ const DestinationImageMapContainer = styled.div`
   }
 `;
 
-const Container = styled.div`
-  padding: 0 1em 0 1em;
-  display: flex;
-  flex-direction: column;
-
-  h2,
-  h3 {
-    text-align: center;
-    margin: 0.5rem 0;
-  }
-
-  h3 {
-    font-weight: normal;
-    font-size: 0.9rem;
-    margin-bottom: 1.2rem;
-  }
-
-  p {
-    margin: 0.5rem 0;
-    font-size: 0.6rem;
-    line-height: 1rem;
-  }
-`;
-
 const DestinationPageMainContent = ({
   title,
   description,
@@ -50,7 +27,7 @@ const DestinationPageMainContent = ({
   children,
 }) => {
   return (
-    <Container>
+    <DestinationPageContainer>
       <h2>{title}</h2>
       <p>{description}</p>
       <h3>{subtitle}</h3>
@@ -64,7 +41,7 @@ const DestinationPageMainContent = ({
         <img src={src} alt={alt} />
         <img src={src} alt={alt} />
       </DestinationImageMapContainer>
-    </Container>
+    </DestinationPageContainer>
   );
 };
 
