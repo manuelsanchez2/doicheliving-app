b0400c1f5d8192afc7687e68e4d6604221d19562
manuelsanchez2 <manusansan22@gmail.com>
2020-09-25 17:52:08 +0200

    Create the getArticle function

--- client/src/api/articles.js
+++ client/src/api/articles.js
@@ -6,3 +6,12 @@ export async function getArticles() {
   const results = await response.json();
   return results;
 }
+
+export async function getArticle(id) {
+  const response = await fetch(`/api/articles/${id}`);
+  if (!response.ok) {
+    throw new Error(response);
+  }
+  const results = await response.json();
+  return results;
+}
