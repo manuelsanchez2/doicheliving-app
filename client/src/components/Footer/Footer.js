77ab04e7d36ce09df02ef36d4e799593cb852bdc
manuelsanchez2 <manusansan22@gmail.com>
2020-09-30 14:14:22 +0200

    Improve footer with useLocation

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import homeSrc from "../../assets/icons/footer-home.svg";
 import mapSrc from "../../assets/icons/footer-map.svg";
 import searchSrc from "../../assets/icons/footer-search.svg";
 import profileSrc from "../../assets/icons/footer-profile.svg";
-
-const ImageFooter = styled.img`
-  width: 2rem;
-  height: 2rem;
-  margin: 12px 10px;
-  padding-bottom: 2.5px;
-`;
 
 const FooterNav = styled.nav`
   box-shadow: var(--shadow-up);
@@ -24,20 +17,47 @@ const FooterNav = styled.nav`
   border-top: 1.5px solid var(--color-dark-border);
 `;
 
+const ImageFooter = styled.img`
+  width: 2rem;
+  height: 2rem;
+  margin: 12px 10px;
+  padding-bottom: 2.5px;
+  opacity: ${({ active }) => (active ? "1" : "0.5")};
+`;
+
 const Footer = () => {
+  const location = useLocation();
   return (
     <FooterNav>
       <Link to="/home">
-        <ImageFooter src={homeSrc} alt="Home" />
+        <ImageFooter
+          src={homeSrc}
+          alt="Home"
+          active={location.pathname === "/home" || location.pathname === "/"}
+        />
       </Link>
       <Link to="/map">
-        <ImageFooter src={mapSrc} alt="Map" />
+        <ImageFooter
+          src={mapSrc}
+          alt="Map"
+          active={location.pathname === "/map"}
+        />
       </Link>
       <Link to="/search">
-        <ImageFooter src={searchSrc} alt="Search Icon" />
+        <ImageFooter
+          src={searchSrc}
+          alt="Search Icon"
+          active={location.pathname === "/search"}
+        />
       </Link>
       <Link to="/login">
-        <ImageFooter src={profileSrc} alt="Profile icon" />
+        <ImageFooter
+          src={profileSrc}
+          alt="Profile icon"
+          active={
+            location.pathname === "/profile" || location.pathname === "/login"
+          }
+        />
       </Link>
     </FooterNav>
   );
